dab31b3dbca786d762579732e69b6ab31bd2e4be
KevinBezerra <kevinmbezerra@gmail.com>
2025-02-21 00:41:40 -0300

    week 7

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Activity running = new Running(3.0, 30);
+        Activity cycling = new Cycling(20.0, 45);
+        Activity swimming = new Swimming(40, 30);
+
+        Console.WriteLine(running.GetSummary());
+        Console.WriteLine(cycling.GetSummary());
+        Console.WriteLine(swimming.GetSummary());
     }
 }
